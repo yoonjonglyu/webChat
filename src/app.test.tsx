bb341eea78e4912445eec3f053e8c48702491a7b
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-11-17 16:26:14 +0900

    feat: APP ENTRY - INIT

---
+++ src/app.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import App from './app';
+
+describe('웹앱 라우트', () => {
+    test('메인 페이지', () => {
+        render(
+            <App />
+        );
+
+        expect(screen.getByText('메인')).toBeInTheDocument();
+    });
+});
