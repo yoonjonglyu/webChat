7fb37dbc2de314171af850d2bec3f6645a91a85d
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-11-18 18:31:24 +0900

    feat(LAYOUT): 레이아웃 - INIT

---
+++ src/components/layout.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface LayoutProps {
+    children?: React.ReactChild
+}
+
+const Layout: React.FC<LayoutProps> = (props) => {
+    const {
+        children
+    } = props;
+    return (
+        <>
+            <header role="header">
+                <h1>WebChat</h1>
+            </header>
+            <main role="main">
+                {children}
+            </main>
+            <footer role="footer">
+                <small>
+                    Copyright 2021. 류윤종 All Right Reserve.
+                </small>
+            </footer>
+        </>
+    );
+}
+
+export default Layout;
