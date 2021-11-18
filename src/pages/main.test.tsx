8e4992c3b456debbbefd6709715e166d1ba46bb6
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-11-18 18:24:46 +0900

    feat(PAGE): MAIN - 기본 레이아웃 구성

--- src/pages/main.test.tsx
+++ src/pages/main.test.tsx
@@ -7,6 +7,9 @@ describe('메인 라우트 시작점', () => {
     test('render', () => {
         render(<Main />);
         
+        expect(screen.getByRole('header')).toHaveTextContent('WebChat');
+        expect(screen.getByRole('main')).toBeVisible();
+        expect(screen.getByRole('footer')).toHaveTextContent('Copyright 2021. 류윤종 All Right Reserve.');
         expect(screen.getByText('메인')).toBeInTheDocument();
     });
 });
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -6,9 +6,19 @@ interface MainProps {
 
 const Main: React.FC<MainProps> = () => {
     return (
-        <div>
-            메인
-        </div>
+        <>
+            <header role="header">
+                <h1>WebChat</h1>
+            </header>
+            <main role="main">
+                메인
+            </main>
+            <footer role="footer">
+                <small>
+                    Copyright 2021. 류윤종 All Right Reserve.
+                </small>
+            </footer>
+        </>
     );
 }
 
