dfc158fa7e77f00c528b568e2f84c22b5f3e6d0c
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-11-18 18:33:09 +0900

    refactor(PAGE): MAIN - 레이아웃 컴포넌트 분리

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 
+import Layout from '../components/layout';
+
 interface MainProps {
 
 }
 
 const Main: React.FC<MainProps> = () => {
     return (
-        <>
-            <header role="header">
-                <h1>WebChat</h1>
-            </header>
-            <main role="main">
-                메인
-            </main>
-            <footer role="footer">
-                <small>
-                    Copyright 2021. 류윤종 All Right Reserve.
-                </small>
-            </footer>
-        </>
+        <Layout>
+            메인
+        </Layout>
     );
 }
 
