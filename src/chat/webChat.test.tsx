75d6ee826446eacef50d0d772b50cba074fadf45
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-11-19 16:28:33 +0900

    feat(CHAT): WEB CHAT - 컴포넌트 기본 UI

--- src/chat/webChat.test.tsx
+++ src/chat/webChat.test.tsx
@@ -5,10 +5,17 @@ import WebChat from './webChat';
 
 describe('webChat 채팅창', () => {
     test('render', () => {
-        render(
+        const { container } = render(
             <WebChat />
         );
+
         expect(screen.getByRole('article')).toBeVisible();
+        const chatBox = container.querySelector('.chat-room');
+        expect(chatBox).toBeVisible();
+        const chatInput = container.querySelector('.chat-form');
+        expect(chatInput).toBeVisible();
+        expect(chatInput?.querySelector('.chat-input')).toBeVisible();
+        expect(chatInput?.querySelector('.chat-request')).toHaveTextContent('전송')
         expect(screen.getByText('채팅창 구현하기')).toBeVisible();
     });
 });
--- src/chat/webChat.tsx
+++ src/chat/webChat.tsx
@@ -7,6 +7,13 @@ interface WebChatProps {
 const WebChat: React.FC<WebChatProps> = () => {
     return (
         <article>
+            <div className="chat-room">
+
+            </div>
+            <form className="chat-form">
+                <input type="text" className="chat-input" />
+                <button type="button" className="chat-request">전송</button>
+            </form>
             채팅창 구현하기
         </article>
     );
