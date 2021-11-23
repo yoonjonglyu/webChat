b292aa5a2ae555d7bdd49567eefb7332f5348f99
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-11-23 15:34:40 +0900

    refactor(CHAT): WEBCHAT - 메시지 전송 폼 분리

--- src/chat/webChat.tsx
+++ src/chat/webChat.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
+import SendForm from './sendForm';
+
 interface WebChatProps {
 
 }
@@ -49,40 +51,11 @@ const WebChat: React.FC<WebChatProps> = () => {
             </div>
         );
     }
-    const Send = (props: any) => {
-        const {
-            socket
-        } = props;
-        const [message, setMessage] = useState('');
-
-        const sendMessage = (e: any) => {
-            e.preventDefault();
-            if (message.length > 0) {
-                socket.emit('send', message);
-            }
-        }
-        const handleMsg = (e: any) => {
-            setMessage(e.target.value);
-            e.target.value = '';
-        }
-
-        return (
-            <form className="chat-form" onSubmit={sendMessage}>
-                <input
-                    type="text"
-                    className="chat-input"
-                    value={message}
-                    onChange={handleMsg}
-                />
-                <button type="button" className="chat-request" onClick={sendMessage}>전송</button>
-            </form>
-        );
-    }
 
     return (
         <article>
             <Log socket={socket} />
-            <Send socket={socket} />
+            <SendForm socket={socket} />
             채팅창 구현하기
         </article >
     );
