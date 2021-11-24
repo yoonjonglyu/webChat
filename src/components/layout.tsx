4394ed3ddaa50fc50c525381afd25cb9fb32d82a
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-11-24 14:30:47 +0900

    feat(LAYOUT): 레이아웃 기본 스타일 - FLEX, COLOR

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,44 @@
 import React from 'react';
+import StyledComponents, { createGlobalStyle } from 'styled-components';
+
+
+const CommonStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        background-color: #EADEDE;
+        // #131313
+    }
+    #app {
+        display: flex;
+        flex-flow: column wrap;
+    }
+`;
+const Header = StyledComponents.header`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 120px;
+    margin: 0;
+    padding: 12px;
+    border-bottom: 1px solid;
+`;
+const Logo = StyledComponents.h1`
+    color: #F58840;
+`;
+const Main = StyledComponents.main`
+    display: flex;
+    justify-content: center;
+    flex-basis: 90vh;
+`;
+const Footer = StyledComponents.footer`
+    display: flex;
+    justify-content: center;
+`;
+const Copy = StyledComponents.small`
+    color: #535252;
+`;
 
 interface LayoutProps {
     children?: React.ReactNode
@@ -10,17 +50,18 @@ const Layout: React.FC<LayoutProps> = (props) => {
     } = props;
     return (
         <>
-            <header role="header">
-                <h1>WebChat</h1>
-            </header>
-            <main role="main">
+            <CommonStyle />
+            <Header role="header">
+                <Logo>WebChat</Logo>
+            </Header>
+            <Main role="main">
                 {children}
-            </main>
-            <footer role="footer">
-                <small>
+            </Main>
+            <Footer role="footer">
+                <Copy>
                     Copyright 2021. 류윤종 All Right Reserve.
-                </small>
-            </footer>
+                </Copy>
+            </Footer>
         </>
     );
 }
