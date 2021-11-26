b415d48dfe0e4ec50bde8d595379da1f1b3a1369
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-11-26 21:35:54 +0900

    fix(CHAT): CHATROOM - 메시지 비제어처리

--- src/chat/chatRoom.tsx
+++ src/chat/chatRoom.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import { Socket } from 'socket.io-client';
 
 interface ChatRoomProps {
@@ -9,11 +9,13 @@ const ChatRoom: React.FC<ChatRoomProps> = (props) => {
     const {
         socket
     } = props;
-    const [chatLog, setChatLog] = useState<Array<any>>([]);
+    const room = useRef(null);
     socket.on('receive', (msg: any) => {
-        const state = Array.from(chatLog);
-        state.push(msg);
-        setChatLog(state);
+        const MessageNode = document.createElement('p');
+        MessageNode.innerText = msg;
+        const root: any = room.current;
+        root.appendChild(MessageNode);
+        // 리덕스 사가 같은 비동기 작업을 대신 처리할 상태관리 로직이 필요하다.
     });
 
     return (
@@ -26,8 +28,8 @@ const ChatRoom: React.FC<ChatRoomProps> = (props) => {
                 border: "1px solid",
                 overflow: "auto",
             }}
+            ref={room}
         >
-            {chatLog.map((msg: any, idx: any) => <p key={idx}>{msg}</p>)}
         </div>
     );
 }
