30b8a1bd376af997a781cf6508b06b6627e28268
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-12-05 18:47:12 +0900

    test(APP): BASE URL 변경에 따른 브라우저 히스토리추가

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-
+import { createBrowserHistory } from 'history';
 import App from './app';
 
 describe('웹앱 라우트', () => {
+    const history = createBrowserHistory();
     test('메인 페이지', () => {
+        history.push('/webChat');
         render(
             <App />
         );
