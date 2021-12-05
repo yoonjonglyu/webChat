2f54f30b0829b5f5d27e76c5b1a6ccdf53b763e6
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-12-05 19:21:53 +0900

    feat(PAGE): 채팅창 컨테이너 & 컨테이너 가로 길이 조정 기능

--- src/pages/main.test.tsx
+++ src/pages/main.test.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import Main from './main';
 
 describe('메인 라우트 시작점', () => {
     test('render', () => {
         render(<Main />);
-        
         expect(screen.getByRole('header')).toHaveTextContent('WebChat');
         expect(screen.getByRole('main')).toBeVisible();
         expect(screen.getByRole('footer')).toHaveTextContent('Copyright 2021. 류윤종 All Right Reserve.');
+
+        
+        expect(screen.getByText('container width'));
+        const container = screen.getByRole('main').querySelector('.container');
+        expect(container).toBeInTheDocument();
+        fireEvent.click(screen.getByText('800px'));
+        expect(screen.getByText('size: 800px'));
+        expect(container).toHaveStyle('max-width: 800px');
+        fireEvent.click(screen.getByText('500px'));
+        expect(screen.getByText('size: 500px'));
+        expect(container).toHaveStyle('max-width: 500px');
+        fireEvent.click(screen.getByText('400px'));
+        expect(screen.getByText('size: 400px'));
+        expect(container).toHaveStyle('max-width: 400px');
     });
 });
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Layout from '../components/layout';
 import ChatConfig from '../chat/chatConfig';
@@ -9,12 +9,46 @@ interface MainProps {
 
 const Main: React.FC<MainProps> = () => {
     const WebChat = ChatConfig({
-        url: 'https://chatting-serve.herokuapp.com/webChat'
+        url: 'http://localhost:444/webChat' || 'https://chatting-serve.herokuapp.com/webChat'
     });
+    const [container, setContainer] = useState<'800' | '500' | '400'>('800');
+
+    const handleContainer = (width: typeof container) => {
+        setContainer(width);
+    }
 
     return (
         <Layout>
-            <WebChat />
+            <section>
+                <ul>
+                    <li><h2>container width</h2></li>
+                    <li><span>size: {container}px</span></li>
+                    <li>
+                        <button type="button" onClick={() => handleContainer('800')}>
+                            800px
+                        </button>
+                    </li>
+                    <li>
+                        <button type="button" onClick={() => handleContainer('500')}>
+                            500px
+                        </button>
+                    </li>
+                    <li>
+                        <button type="button" onClick={() => handleContainer('400')}>
+                            400px
+                        </button>
+                    </li>
+                </ul>
+            </section>
+            <section
+                className="container"
+                style={{
+                    display: "flex",
+                    maxWidth: `${container}px`
+                }}
+            >
+                <WebChat />
+            </section>
         </Layout>
     );
 }
