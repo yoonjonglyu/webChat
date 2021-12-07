b47e306768d45625f455f5b866c2ff5a82e408eb
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-12-07 19:14:59 +0900

    feat(CHAT): 닉네임 응답 - 폼 데이터 핸들링

--- src/chat/sendNickName.tsx
+++ src/chat/sendNickName.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface SendNickNameProps {
-
 }
 
 const SendNickName: React.FC<SendNickNameProps> = () => {
+    const [nickName, setNickName] = useState('');
+    const handleNickName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNickName(e.target.value);
+    }
+    const sendNickName = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(nickName);
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={sendNickName}>
                 <label>대화에 사용하실 닉네임을 입력해주세요.</label>
-                <input type="text" />
+                <input type="text" value={nickName} onChange={handleNickName} />
                 <button type="button">채팅시작</button>
             </form>
         </div>
