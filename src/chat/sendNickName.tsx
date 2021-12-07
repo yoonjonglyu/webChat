7f427673ff0f135a5b27cd0f87a4f8853b4d8901
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-12-07 22:07:05 +0900

    feat(CHAT): 닉네임 응답 컴포넌트 기본UI

--- src/chat/sendNickName.tsx
+++ src/chat/sendNickName.tsx
@@ -14,10 +14,39 @@ const SendNickName: React.FC<SendNickNameProps> = () => {
     }
 
     return (
-        <div>
-            <form onSubmit={sendNickName}>
-                <label>대화에 사용하실 닉네임을 입력해주세요.</label>
-                <input type="text" value={nickName} onChange={handleNickName} />
+        <div
+            style={{
+                display: "flex",
+                flex: "auto",
+                justifyContent: "center",
+                background: "tomato"
+            }}
+        >
+            <form
+                onSubmit={sendNickName}
+                style={{
+                    display: "flex",
+                    flexFlow: "column nowrap",
+                    maxWidth: "50%",
+                    justifyContent: "center"
+                }}
+            >
+                <label
+                    htmlFor="nickName"
+                    style={{
+                        marginBottom: "50px",
+                        textAlign: "center",
+                        fontSize: "0.9rem"
+                    }}
+                >
+                    <h2>대화에 사용하실 닉네임을 입력해주세요.</h2>
+                </label>
+                <input
+                    type="text"
+                    id="nickName"
+                    value={nickName}
+                    onChange={handleNickName}
+                />
                 <button type="button">채팅시작</button>
             </form>
         </div>
