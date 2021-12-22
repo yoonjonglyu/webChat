f04715f3e981a1b00793b5ed62edf08f0381c365
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-12-22 22:07:31 +0900

    feat(TALK): CHAT WINDOW - 채팅창 해더 컴포넌트 바인드

--- src/chat/talk/chatWindow.tsx
+++ src/chat/talk/chatWindow.tsx
@@ -3,6 +3,7 @@ import { Socket } from 'socket.io-client';
 
 import ChatRoom from './chatRoom/chatRoom';
 import SendForm from './sendMessage/sendForm';
+import ChatHead from './chatHead/chatHead';
 
 interface ChatWindowProps {
     socket: Socket
@@ -15,6 +16,10 @@ const ChatWindow: React.FC<ChatWindowProps> = (props) => {
 
     return (
         <>
+            <ChatHead
+                room={"#1"}
+                socket={socket}
+            />
             <ChatRoom socket={socket} />
             <SendForm socket={socket} />
         </>
