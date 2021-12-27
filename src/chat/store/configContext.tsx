bcf46cfd23f453a57f2ecbe88a2ac8a981c6f391
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-12-27 23:23:02 +0900

    feat(STORE): CONFIG - 이미지 사이즈 추가

--- src/chat/store/configContext.tsx
+++ src/chat/store/configContext.tsx
@@ -2,15 +2,26 @@ import React, { createContext, useState } from 'react';
 
 export const ConfigContext = createContext({
   room: "",
-  handleRoom: (data: string) => { }
+  handleRoom: (room: string) => { },
+  imageSize: 500 * 1024,
+  handleImageSize: (imgsize: number) => { },
 });
 
 const ConfigContextProvider: React.FC = ({ children }) => {
   const [room, setRoom] = useState('');
-  const handleRoom = (data: string) => setRoom(data);
+  const handleRoom = (room: string) => setRoom(room);
+  const [imageSize, setImageSize] = useState(500 * 1024);
+  const handleImageSize = (imgsize: number) => setImageSize(imgsize);
 
   return (
-    <ConfigContext.Provider value={{ room, handleRoom }}>
+    <ConfigContext.Provider
+      value={{
+        room,
+        handleRoom,
+        imageSize,
+        handleImageSize
+      }}
+    >
       {children}
     </ConfigContext.Provider>
   );
