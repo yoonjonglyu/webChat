b1676b530e62e7facd26afec641d7657bce18243
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-12-30 23:50:49 +0900

    refactor(CHATROOM): SYSTEM MESSAGE - DOM 구조 수정

--- src/chat/talk/chatRoom/systemMessage.tsx
+++ src/chat/talk/chatRoom/systemMessage.tsx
@@ -6,23 +6,22 @@ interface SystemMessageProps {
 
 const SystemMessage: React.FC<SystemMessageProps> = ({ message }) => {
     return (
-        <div data-testid="system">
-            <span
-                style={{
-                    maxWidth: "100%",
-                    padding: "8px",
-                    margin: "6px",
-                    fontSize: "0.9rem",
-                    color: "rgb(98 14 14)",
-                    wordBreak: "break-word",
-                    background: "rgb(0 0 0 / 19%)",
-                    borderRadius: "6px",
-                    textAlign: "center",
-                }}
-            >
-                {message}
-            </span>
-        </div>
+        <span
+            data-testid="system"
+            style={{
+                maxWidth: "100%",
+                padding: "8px",
+                margin: "6px",
+                fontSize: "0.9rem",
+                color: "rgb(98 14 14)",
+                wordBreak: "break-word",
+                background: "rgb(0 0 0 / 19%)",
+                borderRadius: "6px",
+                textAlign: "center",
+            }}
+        >
+            {message}
+        </span>
     );
 };
 
