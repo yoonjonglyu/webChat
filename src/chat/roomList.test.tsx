faf5bc7b24237567c7bf9da61c0cb0658ccab40b
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-06 01:24:28 +0900

    feat(CHAT): ROOM LIST - 기본 DOM 구조

--- src/chat/roomList.test.tsx
+++ src/chat/roomList.test.tsx
@@ -6,8 +6,20 @@ import RoomList from './roomList';
 describe('채팅방 리스트', () => {
     test('render', () => {
         render(
-            <RoomList />
+            <RoomList
+                rooms={[
+                    '채팅방#1',
+                    '#1',
+                    '#2',
+                    '#3',
+                    '#4'
+                ]}
+            />
         );
-        expect(screen.getAllByTestId('room-list')).toBeInTheDocument();
+        expect(screen.getByTestId('room-list')).toBeInTheDocument();
+        expect(screen.getByText('참여 가능한 채팅방 목록')).toBeInTheDocument();
+        expect(screen.getByTestId('rooms')).toBeInTheDocument();
+        expect(screen.getByText('채팅방#1')).toBeInTheDocument();
+        expect(screen.getByText('#3')).toBeInTheDocument();
     });
 });
--- src/chat/roomList.tsx
+++ src/chat/roomList.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
 interface RoomListProps {
-
+    rooms: Array<any>
 }
 
-const RoomList: React.FC<RoomListProps> = () => {
+const RoomList: React.FC<RoomListProps> = ({ rooms }) => {
     return (
         <article data-testid="room-list">
-
+            참여 가능한 채팅방 목록
+            <ul data-testid="rooms">
+                {
+                    rooms.map((room, idx) =>
+                        <li key={idx}>
+                            {room}
+                        </li>)
+                }
+            </ul>
         </article>
     );
 }
