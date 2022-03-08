2ec0fb8dba0a9b2b4acf855cf770ac5cd58ac8de
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-03-09 05:37:10 +0900

    feat(CHAT HEAD): 인원목록 - 스타일링

--- src/chat/talk/chatHead/chatHead.tsx
+++ src/chat/talk/chatHead/chatHead.tsx
@@ -42,9 +42,11 @@ const ChatHead: React.FC<ChatHeadProps> = (props) => {
                         <li
                             key={idx}
                             style={{
-                                margin: "8px",
+                                margin: "12px",
+                                padding: "8px",
                                 textAlign: "center",
-                                color: "#e4e2b0"
+                                color: "#e4e2b0",
+                                border: "1px solid #969595"
                             }}
                         >
                             {idx + 1} : {id}
