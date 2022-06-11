d0f39671d057c9609513761cb9d1a05059f4804b
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-06-11 17:01:38 +0900

    fix(CHAT): 채팅 참가 플로우 변경 및 채팅상태 코드 상수에 의존

--- src/chat/roomList.tsx
+++ src/chat/roomList.tsx
@@ -4,87 +4,86 @@ import { Socket } from 'socket.io-client';
 import ChatEvents from './lib/chatEvents';
 import { StoreContext } from './store/configureStore';
 
+import ChatStatus from './constants/chatStatus';
+
 interface RoomListProps {
-    rooms: Array<string>
-    socket: Socket
+  rooms: Array<string>;
+  socket: Socket;
 }
 
 const RoomList: React.FC<RoomListProps> = ({ rooms, socket }) => {
-    const [headCount, setHeadCount] = useState<{ [key: string]: Array<string> }>({});
-    const { handleRoom, handleStep } = useContext(StoreContext);
-    const Events = new ChatEvents(socket);
+  const [headCount, setHeadCount] = useState<{ [key: string]: Array<string> }>(
+    {},
+  );
+  const { handleRoom, handleStep } = useContext(StoreContext);
+  const Events = new ChatEvents(socket);
 
-    useEffect(() => {
-        Events.receiveRoomHeadCount(setHeadCount);
-        const healthCheck = setInterval(() => Events.getHeadCount(), 1000);
+  useEffect(() => {
+    Events.receiveRoomHeadCount(setHeadCount);
+    const healthCheck = setInterval(() => Events.getHeadCount(), 1000);
 
-        return () => {
-            clearInterval(healthCheck);
-            setHeadCount({});
-        }
-    }, []);
+    return () => {
+      clearInterval(healthCheck);
+      setHeadCount({});
+    };
+  }, []);
 
-    const joinRoom = (room: string) => {
-        Events.joinRoom(room);
-        handleRoom(room);
-        handleStep(5);
-    }
+  const joinRoom = (room: string) => {
+    Events.joinRoom(room);
+    handleRoom(room);
+    handleStep(ChatStatus.chatroom);
+  };
 
-    return (
-        <article
-            data-testid="room-list"
+  return (
+    <article
+      data-testid='room-list'
+      style={{
+        position: 'absolute',
+        display: 'flex',
+        flex: '1',
+        flexFlow: 'column nowrap',
+        width: '100%',
+        height: '100%',
+        background: 'rgb(255 92 3 / 50%)',
+        overflow: 'auto',
+        textAlign: 'center',
+      }}>
+      <h2
+        style={{
+          padding: '8px',
+          fontSize: '1.5rem',
+          color: '#325332',
+        }}>
+        참여 가능한 채팅방 목록
+      </h2>
+      <ul
+        data-testid='rooms'
+        style={{
+          width: '100%',
+          maxWidth: '500px',
+          margin: '33px auto',
+          padding: 0,
+          listStyle: 'none',
+        }}>
+        {rooms.map((room, idx) => (
+          <li
+            key={idx}
+            onClick={() => joinRoom(room)}
             style={{
-                position: "absolute",
-                display: "flex",
-                flex: "1",
-                flexFlow: "column nowrap",
-                width: "100%",
-                height: "100%",
-                background: "rgb(255 92 3 / 50%)",
-                overflow: "auto",
-                textAlign: "center",
+              margin: '16px',
+              padding: '16px',
+              fontSize: '1.1rem',
+              color: 'whitesmoke',
+              border: '1px solid #fff',
             }}
-        >
-            <h2
-                style={{
-                    padding: "8px",
-                    fontSize: "1.5rem",
-                    color: "#325332"
-                }}
-            >
-                참여 가능한 채팅방 목록
-            </h2>
-            <ul
-                data-testid="rooms"
-                style={{
-                    width: "100%",
-                    maxWidth: "500px",
-                    margin: "33px auto",
-                    padding: 0,
-                    listStyle: "none",
-                }}
-            >
-                {
-                    rooms.map((room, idx) =>
-                        <li
-                            key={idx}
-                            onClick={() => joinRoom(room)}
-                            style={{
-                                margin: "16px",
-                                padding: "16px",
-                                fontSize: "1.1rem",
-                                color: "whitesmoke",
-                                border: "1px solid #fff"
-                            }}
-                            onMouseOver={(e: any) => e.target.style.color = '#dc1f1f'}
-                            onMouseLeave={(e: any) => e.target.style.color = 'whitesmoke'}
-                        >
-                            {room} ({headCount[room]?.length || 0})
-                        </li>)
-                }
-            </ul>
-        </article>
-    );
-}
+            onMouseOver={(e: any) => (e.target.style.color = '#dc1f1f')}
+            onMouseLeave={(e: any) => (e.target.style.color = 'whitesmoke')}>
+            {room} ({headCount[room]?.length || 0})
+          </li>
+        ))}
+      </ul>
+    </article>
+  );
+};
 
 export default RoomList;
--- src/chat/webChat.tsx
+++ src/chat/webChat.tsx
@@ -10,97 +10,66 @@ import Modal from './modal';
 import ChatEvents from './lib/chatEvents';
 import { StoreContext } from './store/configureStore';
 
+import ChatStatus from './constants/chatStatus';
+
 interface WebChatProps {
-    socket: Socket
-    config?: ConfigProps
+  socket: Socket;
+  config?: ConfigProps;
 }
 interface ConfigProps {
-    imageSize?: number
-    secretKey?: string
+  imageSize?: number;
+  secretKey?: string;
 }
-
 
 const WebChat: React.FC<WebChatProps> = (props) => {
-    const {
-        socket,
-        config
-    } = props;
-    const {
-        isModal,
-        handleRoom,
-        handleImageSize,
-        handleSecretKey,
-        step,
-        handleStep
-    } = useContext(StoreContext);
-    useEffect(() => {
-        if (config?.imageSize) handleImageSize(config.imageSize);
-        if (config?.secretKey) handleSecretKey(config.secretKey);
-    }, []);
+  const { socket, config } = props;
+  const { isModal, handleImageSize, handleSecretKey, step, handleStep } =
+    useContext(StoreContext);
+  useEffect(() => {
+    if (config?.imageSize) handleImageSize(config.imageSize);
+    if (config?.secretKey) handleSecretKey(config.secretKey);
+  }, []);
 
-    const Events = new ChatEvents(socket);
-    const [rooms, setRooms] = useState<Array<string>>([]);
-    useEffect(() => {
-        Events.handleConnect(() => {
-            Events.getRooms(setRooms);
-        });
-        Events.handleDisConnect(() => {
-            handleStep(1);
-        });
-        Events.handleError(() => {
-            handleStep(1);
-        });
-        return () => {
-            socket.close();
-        }
-    }, [socket]);
-    useEffect(() => {
-        if (rooms.length > 0) {
-            if (rooms.length === 1) {
-                Events.joinRoom(rooms[0]);
-                handleRoom(rooms[0]);
-                handleStep(5);
-            } else {
-                handleStep(3);
-            }
-        }
-    }, [rooms]);
+  const Events = new ChatEvents(socket);
+  const [rooms, setRooms] = useState<Array<string>>([]);
+  useEffect(() => {
+    Events.handleConnect(() => {
+      handleStep(ChatStatus.waitRoom);
+      Events.getRooms(setRooms);
+    });
+    Events.handleDisConnect(() => {
+      handleStep(ChatStatus.error);
+    });
+    Events.handleError(() => {
+      handleStep(ChatStatus.error);
+    });
+    return () => {
+      socket.close();
+    };
+  }, [socket]);
+  useEffect(() => {
+    if (rooms.length > 0) handleStep(ChatStatus.setNickName);
+  }, [rooms]);
 
-    return (
-        <article style={{
-            display: "flex",
-            flexFlow: "column wrap",
-            flex: "1",
-            justifyContent: "center",
-            position: "relative",
-            border: "1px solid #678983",
-        }}
-        >
-            {
-                isModal &&
-                <Modal />
-            }
-            {
-                step < 2 &&
-                <Loading state={step} />
-            }
-            {
-                step === 3 &&
-                <RoomList
-                    rooms={rooms}
-                    socket={socket}
-                />
-            }
-            {
-                step === 4 &&
-                <SendNickName />
-            }
-            {
-                step > 4 &&
-                <ChatWindow socket={socket} />
-            }
-        </article >
-    );
-}
+  return (
+    <article
+      style={{
+        display: 'flex',
+        flexFlow: 'column wrap',
+        flex: '1',
+        justifyContent: 'center',
+        position: 'relative',
+        border: '1px solid #678983',
+      }}>
+      {isModal && <Modal />}
+      {step < ChatStatus.setNickName && <Loading state={step} />}
+      {step === ChatStatus.setNickName && <SendNickName />}
+      {step === ChatStatus.roomList && (
+        <RoomList rooms={rooms} socket={socket} />
+      )}
+      {step > ChatStatus.chatroom && <ChatWindow socket={socket} />}
+    </article>
+  );
+};
 
 export default WebChat;
