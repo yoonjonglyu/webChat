e97ad57506fbf17c39eec3b427e6a80b01069e40
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-06-12 21:22:01 +0900

    feat(STORE): USER INFO - 사용자 정보 닉네임 상태

--- src/chat/store/system/index.ts
+++ src/chat/store/system/index.ts
@@ -1,5 +1,6 @@
 import * as Step from './step';
 import * as Room from './room';
 import * as RoomList from './roomList';
+import * as UserInfo from './userInfo';
 
-export default [Step, Room, RoomList];
+export default [Step, Room, RoomList, UserInfo];
