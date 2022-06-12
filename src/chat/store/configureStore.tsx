4db32f6c3e3a5786fa49f371104d3ac50cd0d333
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-06-12 21:07:26 +0900

    refactor(STORE): 루트 상태 타입 지정

--- src/chat/store/configureStore.tsx
+++ src/chat/store/configureStore.tsx
@@ -2,27 +2,32 @@ import React, { createContext } from 'react';
 import RootState from './rootState';
 
 export const StoreContext: React.Context<any> = createContext({
-    ...RootState.reduce((result, context) => ({
-        ...result,
-        ...context.initState,
-    }), {})
+  ...RootState.reduce(
+    (result, context) => ({
+      ...result,
+      ...context.initState,
+    }),
+    {},
+  ),
 });
 
 const ConfigureStore: React.FC = ({ children }) => {
-    const state = RootState.reduce((result, context) => ({
-        ...result,
-        ...context.setContext(),
-    }), {});
-    
-    return (
-        <StoreContext.Provider
-            value={{
-                ...state
-            }}
-        >
-            {children}
-        </StoreContext.Provider>
-    );
+  const state = RootState.reduce(
+    (result, context) => ({
+      ...result,
+      ...context.setContext(),
+    }),
+    {},
+  );
+
+  return (
+    <StoreContext.Provider
+      value={{
+        ...state,
+      }}>
+      {children}
+    </StoreContext.Provider>
+  );
 };
 
 export default ConfigureStore;
--- src/chat/store/rootState.tsx
+++ src/chat/store/rootState.tsx
@@ -2,11 +2,10 @@ import Modal from './modal';
 import System from './system';
 import Config from './config';
 
-const RootState = [
-    ...Modal,
-    ...System,
-    ...Config,
+const RootState: Array<{ initState: Object; setContext: Function }> = [
+  ...Modal,
+  ...System,
+  ...Config,
 ];
-
 
 export default RootState;
